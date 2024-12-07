aff98f27a24a5de5ebe795cda2baafc6da56e5af
Ryan Waldheim <ryan@waldhe.im>
2024-12-07 18:56:38 -0500

    day3.p2

--- code/day3/src/main.rs
+++ code/day3/src/main.rs
@@ -36,4 +36,43 @@ fn main() {
         total_mul_result,
         start_time.elapsed().as_micros()
     );
+
+    // -- Part 2 --
+
+    // Restart the timer
+    let start_time = Instant::now();
+
+    // Also find the "do()" and "don't()" expressions
+    let mul_expression = Regex::new(r"do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)").unwrap();
+
+    let mut total_mul_result: i32 = 0;
+    let mut vector_match: Vec<&str> = Vec::new();
+    let mut include_mul = true;
+    for found in mul_expression.captures_iter(&contents) {
+        let found_match = found.get(0).unwrap().as_str();
+
+        // Let the presence of "do()" and "don't()" determine whether to include the "mul()" expressions
+        if found_match == "do()" {
+            include_mul = true;
+        } else if found_match == "don't()" {
+            include_mul = false;
+        } else if found_match.starts_with("mul(") && include_mul {
+            let digits: Vec<_> = digits_expression
+                .captures_iter(found_match)
+                .map(|cap| cap.get(0).unwrap().as_str().parse::<i32>().unwrap())
+                .collect();
+
+            if digits.len() == 2 {
+                total_mul_result += digits[0] * digits[1];
+            }
+
+            vector_match.push(found_match);
+        }
+    }
+
+    println!(
+        "Total mul result: {} in {} ns",
+        total_mul_result,
+        start_time.elapsed().as_micros()
+    );
 }
