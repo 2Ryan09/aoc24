f5410e4e9f854d0b2b993558f4a7832d1a9fb627
Ryan Waldheim <ryan@waldhe.im>
2024-12-12 17:47:17 -0500

    wip

--- code/day4/src/main.rs
+++ code/day4/src/main.rs
@@ -13,36 +13,67 @@ fn main() {
         .collect();
 
     let word = "XMAS";
-    let count = find_word_in_grid(&content_grid, word);
+    let count = find_word_in_grid(&content_grid, word, false);
 
     // First star solution
     println!("Word '{}' found {} times", word, count);
+
+    let word = "MAS";
+    let count = find_word_in_grid(&content_grid, word, true);
+
+    // Second star solution
+    println!("X-MAS found {} times", count);
 }
 
-fn find_word_in_grid(grid: &Vec<Vec<char>>, word: &str) -> usize {
-    let directions = vec![
+fn find_word_in_grid(grid: &Vec<Vec<char>>, word: &str, x_only: bool) -> usize {
+    let cardinal_directions = vec![
         (0, 1),  // right
         (0, -1), // left
         (1, 0),  // down
-        (-1, 0), // up
+        (-1, 0)  // up
+    ];
+
+    let diagonal_directions = vec![
         (1, 1),  // down-right
         (1, -1), // down-left
         (-1, 1), // up-right
         (-1, -1) // up-left
     ];
 
+    let directions: Vec<(isize, isize)> = if x_only {
+        diagonal_directions
+    } else {
+        let mut all_directions = cardinal_directions;
+        all_directions.extend(diagonal_directions);
+        all_directions
+    };
+
     let rows = grid.len();
     let cols = grid[0].len();
     let word_len = word.len();
+    let mut idx_count = 0;
     let mut count = 0;
 
     for i in 0..rows {
         for j in 0..cols {
             for &(dx, dy) in &directions {
                 if check_word(grid, word, i, j, dx, dy, word_len) {
-                    count += 1;
+                    idx_count += 1;
                 }
             }
+
+            if x_only && (idx_count == 2) {
+                count += 1;
+                idx_count = 0;
+
+                continue;
+            } else if x_only {
+                idx_count = 0;
+                continue;
+            }
+
+            count += idx_count;
+            idx_count = 0;
         }
     }
 
